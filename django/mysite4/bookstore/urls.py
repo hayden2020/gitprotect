819f867ed0deab772d5375e6ccf8031f6db4a2ce
hayden <1345608604@qq.com>
2020-08-20 00:20:41 +0800

    django/

--- django/mysite4/bookstore/urls.py
+++ django/mysite4/bookstore/urls.py
@@ -4,4 +4,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url(r'^add',views.add_view),
+    url(r'^all',views.show_all),  # all 表示网页地址
+    url(r'^mod/(\d+)',views.mod_view),
+    url(r'^del/(\d+)',views.del_view),
+
 ]
--- django/mysite4/bookstore/views.py
+++ django/mysite4/bookstore/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
+from django.http import HttpResponseRedirect  # 重定向
 # Create your views here.
 
 # file:bookstore/views.py
@@ -35,6 +36,37 @@ def add_view(request):
             price=price,
             market_price=market_price
         )
-        return HttpResponse("添加成功")
+        return HttpResponseRedirect('/bookstore/all')
     except:
         return HttpResponse("添加失败")
+
+def show_all(request):
+    books=models.Book.objects.all()  # 查询所有的图书
+    #books = models.Book.objects.filter(price__range=(50,80))  # 查询50到80的价格
+    #books = models.Book.objects.filter(price__lte=80)
+    # for abook in books:
+    #     print("书名"+abook.title)
+    # return HttpResponse("查询成功")
+
+    return render(request,'bookstore/list.html',locals())
+
+def mod_view(request,id):
+    try:
+        abook=models.Book.objects.get(id=id)
+    except :
+         return HttpResponse("没有id为" + id +"的数据记录")
+    if request.method=='GET':
+        return render(request,'bookstore/mod.html',locals())
+    elif request.method=='POST':
+        market_price=float(request.POST.get('market_price',))  #
+        abook.market_price=market_price   # 修改字段的值
+        abook.save()
+        return HttpResponseRedirect('/bookstore/all')  # 修改成功后u返回指定 的页面
+
+def del_view(request,id):
+    try:
+        abook=models.Book.objects.get(id=id)
+    except Exception as err:
+        return HttpResponse("删除失败")
+    abook.delete()
+    return HttpResponseRedirect('/bookstore/all')
