3f7efc34d2ba89823fac19af33c9a9c0aebd1225
hayden <1345608604@qq.com>
2020-08-23 22:39:06 +0800

    -A

--- django/mysite4/bookstore/admin.py
+++ django/mysite4/bookstore/admin.py
@@ -1,3 +1,24 @@
+# file:bookstore/admin
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+class BookManager(admin.ModelAdmin):   # 模型管理器类,可以为后台管理添加功能
+    list_display = ['id','title','pub','price','market_price']
+    list_display_links = ['id','title']
+    list_filter = ['pub']
+    search_fields = ['title','pub']
+    list_editable = ['market_price']
+    #list_per_page = ['pub']
+class AuthorManager(admin.ModelAdmin):
+    list_display=['id','name','age']
+    list_display_links = ['id', 'name']
+    list_filter = ['name']
+    search_fields = ['id', 'name']
+class WifeManager(admin.ModelAdmin):
+    list_display=['id','name','author']
+admin.site.register(models.Book,BookManager)
+
+admin.site.register(models.Author,AuthorManager)
+admin.site.register(models.Wife,WifeManager)
--- django/mysite4/bookstore/models.py
+++ django/mysite4/bookstore/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    title=models.CharField(max_length=30,db_index=True,unique=True) # 等于varchar(30)
+    title=models.CharField(max_length=30,db_index=True,unique=True,
+                           verbose_name='书名') # 等于varchar(30)
     pub = models.CharField(max_length=50, verbose_name='出版社', null=True, )
     price = models.DecimalField(decimal_places=2, max_digits=7,
                                 default=999, verbose_name='定价')
@@ -11,9 +12,22 @@ class Book(models.Model):
                                        decimal_places=2,
                                        verbose_name='零售价',
                                        default=88, )  # 等于 Decimal(7，2)
+    def __str__(self):
+        return "书名:"+self.title
+    class Meta:
+       #db_table='mybook'  # 修改数据表名字
+        pass
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30,verbose_name='姓名')
     age = models.IntegerField(verbose_name='年龄')  # IntegerField 姓名非空
-    email = models.EmailField(verbose_name='邮箱')  # 邮箱允许
+    email = models.EmailField(verbose_name='邮箱',default='xxx@yyy.zzz')  # 邮箱允许
+    def __str__(self):
+        return "作者："+self.name
+
+
+class Wife(models.Model):
+    name=models.CharField(max_length=30,
+                          verbose_name='姓名')
+    author=models.OneToOneField(Author)
